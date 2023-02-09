14150f12ebda604a7f4514115db29a81345a667f
Dinesh Jothiram <dinesh.jothiram@insofe.edu.in>
2023-02-09 12:14:09 +0530

    change to 80

--- model.py
+++ model.py
@@ -8,7 +8,7 @@ df=pd.read_csv('diabetes.csv')
 X=df.iloc[:,:-1]
 Y=df.iloc[:,-1]
 
-X_train,X_test,y_train,y_test=train_test_split(X,Y,test_size=0.9,random_state=0)
+X_train,X_test,y_train,y_test=train_test_split(X,Y,test_size=0.8,random_state=0)
 
 classifier=RandomForestClassifier()
 classifier.fit(X_train,y_train)
